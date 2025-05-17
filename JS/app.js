e277d1c9272de727bce852455c2e18203ec73b40
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-05-17 09:05:22 -0500

    battleship beginning

---
+++ JS/app.js
@@ -0,0 +1,39 @@
+let item1 = document.querySelector(".item item1");
+item1.addEventListener("click", function() {
+    if (.correct){
+  item1.style.backroundColor = "red";{
+ } else if
+});
+let item2 = document.querySelector(".item item2");
+item2.addEventListener("click", function() {
+    if (.correct){
+  item1.style.backroundColor = "red";
+});
+
+
+
+}
+.item item2{
+    
+}
+.item item3{
+
+}
+.item item4{
+
+}
+.item item5{
+
+}
+.item item6{
+
+}
+.item item7{
+
+}
+.item item8{
+
+}
+.item item9{
+
+}
